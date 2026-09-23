462715626cccc5643210c14ecd6c4bc108a28032
Senz Digital Sol <senzdigitals.inc23@gmail.com>
2026-09-23 16:10:07 +0000

    Scaffold setup partialliy done

--- Core/MiddlewareInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace App\Core;
-
-interface MiddlewareInterface
-{
-    public function handle(Request $request, Response $response, callable $next): Response;
-}
--- Services/reports/ReportInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Services\Reports;
-
-interface ReportInterface {
-    public function generate(array $data, string $filePath): string;
-}
