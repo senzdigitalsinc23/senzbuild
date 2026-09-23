51b8b23c262605a9754172de8608fe73b06bb127
Senz Digital Sol <senzdigitals.inc23@gmail.com>
2026-09-23 16:20:53 +0000

    Scaffold setup partialliy done

--- App/CLI/CommandInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace App\CLI;
-
-interface CommandInterface
-{
-    public function handle(array $args): void;
-}
--- Core/ShouldQueue.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace App\Core;
-
-interface ShouldQueue
-{
-    // Marker interface; any Job implementing this will be queued
-}
